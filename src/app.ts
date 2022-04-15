4cd34290d12637096b0311e85425ed3deaed0bb5
Krusharska,Yulia Y <y.krusharska@student.fontys.nl>
2022-04-15 18:54:05 +0200

    Add RabbitMq

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client'
 import routes from './routes'
+// import amqp from "amqplib/callback_api";
+
 
 const app = express();
 
@@ -16,7 +18,7 @@ app.use(express.urlencoded({
     res.send('Events microservice has been started');
   })
 
-const port = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 3000;
+const port = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 3002;
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
@@ -27,6 +29,21 @@ const prisma = new PrismaClient()
 async function main() {
   console.log("Events Microservice has been started!");
 
+  // const CONN_URL = 'amqp://guest:guest@rabbitmq:5672';
+  // amqp.connect(CONN_URL, function (err, conn) {
+  //   conn.createChannel(function (err, ch) {
+  //     ch.consume('example', function (msg) {
+  //       console.log('.....');
+  //       setTimeout(function(){
+  //         if(msg){
+  //         console.log("Message:", msg.content.toString());
+  //         ch.ack(msg);
+  //         }
+  //       },4000);
+  //       },{ noAck: true }
+  //     );
+  //   });
+  // });
   // await prisma.event.create({
   //   data: {
   //     eventName: "Running in heels",
